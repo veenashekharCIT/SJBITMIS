f213aeca75b7c6141fbeb744cb2f8d4bf9c625e1
Student <swetha@creativeinfotech.in>
2017-01-30 12:22:06 +0530

    i added hello

--- SJBITMIS/SJBITMIS/ViewController.swift
+++ SJBITMIS/SJBITMIS/ViewController.swift
@@ -13,10 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //hello
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 }
